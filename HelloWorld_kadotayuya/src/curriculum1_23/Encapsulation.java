949bb0050500fce55e0e0b96c128a2bbf7dcdafa
yuya <yuya@localhost>
2025-05-22 17:44:39 +0900

    1-23

--- HelloWorld_kadotayuya/src/curriculum1_23/Encapsulation.java
+++ HelloWorld_kadotayuya/src/curriculum1_23/Encapsulation.java
@@ -1,39 +1,50 @@
+// パッケージ宣言：このクラスが属するグループ（フォルダのようなもの）
 package curriculum1_23;
 
+/**
+ * カプセル化の例として使われるクラス。 名前・身長・速度を持ち、それぞれにgetter/setterを用意している。
+ */
 public class Encapsulation {
 
+	// フィールド（データ）：外部から直接アクセスできないようにprivateで隠す（カプセル化）
 	private String name;
 	private double height;
-	private int spead;
+	private int spead; // ← 速度（スペルは「speed」が正しい）
 
+	// コンストラクタ：オブジェクト生成時にフィールドへ初期値を代入
 	public Encapsulation(String name, double height, int spead) {
 		this.name = name;
 		this.height = height;
 		this.spead = spead;
 	}
 
+	// 名前を取得するメソッド（getter）
 	public String getName() {
 		return this.name;
 	}
 
+	// 名前を変更するメソッド（setter）
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	// 身長を取得するgetter
 	public double getHeight() {
 		return height;
 	}
 
+	// 身長を設定するsetter
 	public void setHeight(double height) {
 		this.height = height;
 	}
 
+	// 速度を取得するgetter
 	public int getSpead() {
 		return spead;
 	}
 
+	// 速度を設定するsetter
 	public void setSpead(int spead) {
 		this.spead = spead;
 	}
-
 }
--- HelloWorld_kadotayuya/src/curriculum1_23/Main.java
+++ HelloWorld_kadotayuya/src/curriculum1_23/Main.java
@@ -1,23 +1,16 @@
+// パッケージ宣言：このクラスが所属するフォルダ（グループ）を表す
 package curriculum1_23;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Encapsulation lion = new Encapsulation("らいおーん", 2.1, 80); //値を代入
+    public static void main(String[] args) {
+        // Encapsulationクラスのインスタンス（オブジェクト）を作成
+        // 「らいおーん」という名前、体長2.1m、速度80km/h の値を渡している
+        Encapsulation lion = new Encapsulation("らいおーん", 2.1, 80);
 
-		System.out.println("動物名：" + lion.getName()); //
-		System.out.println("体長：" + lion.getHeight() + "m"); //
-		System.out.println("速度：" + lion.getSpead() + "km/h"); //
-
-	}
+        // オブジェクトからgetterを使って情報を取得し、出力
+        System.out.println("動物名：" + lion.getName());         // 名前を表示
+        System.out.println("体長：" + lion.getHeight() + "m");   // 体長を表示（単位：メートル）
+        System.out.println("速度：" + lion.getSpead() + "km/h"); // 速度を表示（単位：km/h）
+    }
 }
-/*
-	
-	下記がコンソールに出力されるように作成してください
-	※thisとsetterとgetterとフィールドを使ってください
-	
-	動物名：ライオン
-体長：2.1m
-速度：80km/h
-	
-*/
